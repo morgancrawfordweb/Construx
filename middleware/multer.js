715858659104d9275fa108668d30c9bc5c07c098
Morgan Crawford <101520419+morgancrawfordweb@users.noreply.github.com>
2023-03-07 19:23:52 -0800

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-    if (ext !== ".pdf" || ext !==".jpg") {
+    if (ext !== ".pdf") {
       cb(new Error("File type is not supported, try using a pdf!"), false);
       return;
     }
