790952be92fa273461aa4a5f5c0b665c825c358d
Morgan Crawford <macrawfo94@gmail.com>
2024-08-13 21:03:39 -0400

    some message

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const projectsController = require("../controllers/projects");
+const forgotPasswordController = require("../controllers/forgotPassword");
 const { ensureAuth, ensureCompanyAuth, ensureGuest } = require("../middleware/auth");
 const companyController = require("../controllers/company")
 const companyAuthController = require("../controllers/companyAuth");
-const subscriptionsController = require("../controllers/subscriptions")
 
 
 
-//Get the subscription page
-// router.get("/subscriptionSignup", ensureCompanyAuth, subscriptionsController.getSubscription);
+// app.post("/create-subscription", ensureCompanyAuth, subscriptionController.createSubscription)
+// app.get("/subscription-page", ensureCompanyAuth, subscriptionController,getSubscriptionPage)
+
 
 
 
