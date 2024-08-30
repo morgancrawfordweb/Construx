abe7f0f268de73f291a1825651b007ac6ce33ddb
unknown <macrawfo94@gmail.com>
2024-08-29 17:49:50 -0700

    clarified project.ejs and addedmore info on instructional

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,15 +4,16 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
+    let ext = path.extname(file.originalname).toLowerCase();
     if(ext !==".pdf" && ext !==".png" && ext !==".img"&& ext !==".jpg"&& ext !==".jpeg"){
-      cb(new Error("File type is not supported, try using a pdf!"), false);
+      cb(new Error("File type is not supported, try using one of these jpg, png, img, pdf, or jpeg"), false);
       return;
     }
     cb(null, true);
   },
 });
 
+// "jpg", "png", "pdf","jpeg","xlsx"
 
 // module.exports = ({
 //  storage = multer.diskStorage({
