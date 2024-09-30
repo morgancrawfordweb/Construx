fcdb91e59ca0a43b9c05217e710c154a5ae3afd3
Morgan Crawford <macrawfo94@gmail.com>
2024-09-30 01:59:21 -0400

    mailgun info

--- controllers/company.js
+++ controllers/company.js
@@ -85,14 +85,14 @@ module.exports = {
       const formData = require('form-data');
       const Mailgun = require('mailgun.js');
       const mailgun = new Mailgun(formData);
-      const mg = mailgun.client({username: 'morgancrawfordweb', key: process.env.MAILGUN_API_KEY});
+      const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || '7a3af442-e0a34ea2'});
       
-      mg.messages.create('mailgun@sandbox6a15524aaaa24af5b221d872075afd9c.mailgun.org', {
+      mg.messages.create('sandbox-123.mailgun.org', {
         from: "Excited User <mailgun@sandbox6a15524aaaa24af5b221d872075afd9c.mailgun.org>",
-  	to: [`${newUser}`],
-  	subject: "Hello",
-  	text: `Testing some Mailgun awesomeness! ${signupUrl}`,
-  	html: "<h1>Testing some Mailgun awesomeness!</h1>"
+        to: ["test@example.com"],
+        subject: "Hello",
+        text: "Testing some Mailgun awesomeness!",
+        html: "<h1>Testing some Mailgun awesomeness!</h1>"
       })
   
   
