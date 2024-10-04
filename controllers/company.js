fa15803c13d301023645ae5836ae18afb998a917
Morgan Crawford <macrawfo94@gmail.com>
2024-10-04 07:18:28 -0400

    some message

--- controllers/company.js
+++ controllers/company.js
@@ -88,7 +88,7 @@ module.exports = {
       const formData = require('form-data');
       const Mailgun = require('mailgun.js');
       const mailgun = new Mailgun(formData);
-      const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY}); 
+      const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
       
       mg.messages.create('sandbox-123.mailgun.org', {
         from: "Excited User <mailgun@sandbox6a15524aaaa24af5b221d872075afd9c.mailgun.org>",
